948c85390f780047840127dde985b8e302bc2de5
johnzavax <johndaniel1st@gmail.com>
2024-07-23 20:12:19 +0530

    added Data type & identifier

---
+++ C2ct_project/src/c2ct/sample/pro1/DataType.java
@@ -0,0 +1,19 @@
+package c2ct.sample.pro1;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		int value1 = 9/2;
+		float value2=9/2;
+		double value3=9d/2d;
+		System.out.println("value 1= "+ value1);
+		System.out.println("value 2= "+ value2);
+		System.out.println("value 3= "+ value3);
+		int pen=300;
+		double percentage=pen * 0.50;
+		System.out.println("value 4= "+ percentage);
+
+
+	}
+
+}
---
+++ C2ct_project/src/c2ct/sample/pro1/Identifier.java
@@ -0,0 +1,12 @@
+package c2ct.sample.pro1;
+
+public class Identifier {
+
+	public static void main(String[] args) {
+		int $number = 20;
+		System.out.println("value of the number is : "+ $number);
+		String studentName="john";
+		System.out.println("Name of the student: "+studentName);
+	}
+
+}
