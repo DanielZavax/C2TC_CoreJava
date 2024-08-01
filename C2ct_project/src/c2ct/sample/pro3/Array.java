ba59f59a04b6ee37bf29fd988a17d58acb3bb78e
johnzavax <johndaniel1st@gmail.com>
2024-08-01 19:50:36 +0530

    commit

--- C2ct_project/src/c2ct/sample/pro3/Array.java
+++ C2ct_project/src/c2ct/sample/pro3/Array.java
@@ -9,7 +9,7 @@ public class Array {
 	
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
--- C2ct_project/src/c2ct/sample/pro3/Pattern.java
+++ C2ct_project/src/c2ct/sample/pro3/Pattern.java
@@ -3,7 +3,7 @@ package c2ct.sample.pro3;
 public class Pattern {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
